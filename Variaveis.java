59d41168e57de7febe4cb370410cf55f8092990c
michellopes338 <lopesmichel489@gmail.com>
2022-10-11 22:07:03 -0300

    trabalhando com variaveis

---
+++ Variaveis.java
@@ -0,0 +1,13 @@
+import java.text.MessageFormat;
+import java.util.Date;
+
+public class Variaveis {
+  public static void main(String[] args) {
+    short anoDeNascimento = 2001;
+    int anoAtual = java.time.LocalDate.now().getYear();
+    int idade = anoAtual - anoDeNascimento;
+    String nome = "Michel";
+    String mensagem = MessageFormat.format("Olá, meu nome é {0} e eu tenho {1} anos de idade", nome, idade);
+    System.out.println(mensagem);
+  }
+}
