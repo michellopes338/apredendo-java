e73e16ed8ed1e0dca1f0620f9ce61b714b9c3f75
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 16:19:52 -0300

    exercicio laço for

---
+++ ExercicioLaco.java
@@ -0,0 +1,12 @@
+/**
+ * ExercicioLaco
+ * ##Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
+ */
+public class ExercicioLaco {
+
+    public static void main(String[] args) {
+        for (byte multiplos = 3; multiplos <= 100; multiplos += 3) {
+            System.out.println(multiplos);
+        }
+    }
+}
