10dfdbaaf5ef3526cad0f2927366cf455a216885
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 01:51:44 -0300

    caracteres em java

---
+++ Caracteres.java
@@ -0,0 +1,14 @@
+/**
+ * Caracteres
+ */
+public class Caracteres {
+
+    public static void main(String[] args) {
+        char letra = 65;
+        System.out.println(letra);
+        letra = (char) (letra + 1);
+        System.out.println(letra);
+        String empresa = "Minha Empresa LTDA";
+        System.out.println(empresa);
+    }
+}
