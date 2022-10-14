45b0c073e4c65dc8b6745f29fd90cfc235a2e6fb
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 01:54:19 -0300

    caracteres em java

--- Caracteres.java
+++ Caracteres.java
@@ -4,11 +4,15 @@
 public class Caracteres {
 
     public static void main(String[] args) {
+        // char (tipo primitivo)
         char letra = 65;
-        System.out.println(letra);
-        letra = (char) (letra + 1);
-        System.out.println(letra);
+        System.out.println(letra); // A
+        letra = (char) (letra + 1); // muda de caractere de acordo com a tabela unicode
+        System.out.println(letra); // B
+
+        // String
         String empresa = "Minha Empresa LTDA";
-        System.out.println(empresa);
+        empresa = empresa + 2020; // concatena
+        System.out.println(empresa); // Minha Empresa LTDA2020
     }
 }
