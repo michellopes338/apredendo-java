61868c556bc425b303ba6cdb601f3491fd0ac078
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 16:17:42 -0300

    lacos for

--- Lacos.java
+++ Lacos.java
@@ -4,13 +4,29 @@
 public class Lacos {
 
     public static void main(String[] args) {
-        byte contador = 0;
-        int total = 0;
-        while (contador <= 10) {
-            System.out.println("impressão n " + contador);
-            total += contador;
-            System.out.println(total);
-            contador++;
+        // WHILE
+        // byte contador = 0;
+        // int total = 0;
+        // while (contador <= 10) {
+        //     System.out.println("impressão n " + contador);
+        //     total += contador;
+        //     System.out.println(total);
+        //     contador++;
+        // }
+
+        // FOR
+        // for (int contador = 0; contador < 10; contador++) {
+        //     System.out.println("impressão n " + contador);
+        // }
+
+        for (int linha = 1; linha < 10; linha++) {
+            for (int coluna = 1; coluna < 10; coluna++) {
+                if (coluna > linha) {
+                    break;
+                }
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
