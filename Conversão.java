df334a93e432106a11bef8c2830c38b0210b4e34
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 01:19:35 -0300

    aprendendo conversão

---
+++ Conversão.java
@@ -0,0 +1,13 @@
+/**
+ * Conversão
+ */
+public class Conversão {
+
+    public static void main(String[] args) {
+        double salario = 1212.5;
+        // (int) converte o valor a direita do "=" de double para inteiro
+        int valor = (int) salario;
+
+        System.out.print(valor);
+    }
+}
