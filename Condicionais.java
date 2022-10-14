17ba754cefc92bf0f1a47b1336af492f6a817d69
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 02:30:06 -0300

    melhorando a condiciona com condicionais

--- Condicionais.java
+++ Condicionais.java
@@ -9,17 +9,13 @@ public class Condicionais {
         Scanner leitor = new Scanner(System.in);
         System.out.println("Quantos anos vc tem? ");
         byte idade = leitor.nextByte();
-        if (idade >= 18) {
-            System.out.println("Vc já é maior de idade");
+        System.out.println("Está acompanhado por quantas pessoas? ");
+        byte acompanhantes = leitor.nextByte();
+
+        if (idade >= 18 || acompanhantes >= 2) {
             System.out.println("Seja bem vindo");
         } else {
-            System.out.println("Está acompanhado por quantas pessoas? ");
-            byte acompanhantes = leitor.nextByte();
-            if (acompanhantes <= 0) {
-                System.out.println("Infelizmente vc não pode entrar");
-            } else {
-                System.out.println("Tudo bem, pode entrar!");
-            }
+            System.out.println("Tudo bem, pode entrar!");
         }
         leitor.close();
     }
